f99055485d6fff7273642ffbed13cb8016c9cc27
Mayank Bavankar <mayanksahu1053@gmail.com>
2024-09-20 15:50:17 +0530

    refactor: Update generativeAI service configuration

--- Backend/src/services/generativeAI.service.js
+++ Backend/src/services/generativeAI.service.js
@@ -7,88 +7,98 @@ import {
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-const lessonGenerationConfig = {
-  temperature: 1,
-  topP: 0.95,
-  topK: 64,
-  maxOutputTokens: 8192,
-  responseMimeType: "application/json",
-  responseSchema: {
-    type: "object",
-    properties: {
-      Title: {
-        type: "string",
-      },
-      Plan: {
-        type: "array",
-        items: {
-          type: "object",
-          properties: {
-            topic: {
-              type: "string",
-            },
-            subtopics: {
-              type: "array",
-              items: {
-                type: "object",
-                properties: {
-                  title: {
-                    type: "string",
+const lessonPlanGenerationConfig = {
+  model: "gemini-1.5-pro-exp-0827",
+  generationConfig: {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "application/json",
+    responseSchema: {
+      type: "object",
+      properties: {
+        Title: {
+          type: "string",
+        },
+        Plan: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              topic: {
+                type: "string",
+              },
+              subtopics: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    title: {
+                      type: "string",
+                    },
                   },
+                  required: ["title"],
                 },
-                required: ["title"],
               },
             },
+            required: ["topic", "subtopics"],
           },
-          required: ["topic", "subtopics"],
         },
       },
+      required: ["Title", "Plan"],
     },
-    required: ["Title", "Plan"],
   },
+  systemInstruction:
+    "As an expert in educational content creation, your assignment is to craft three structured lesson plans in JSON format tailored to the user's specifications. The user will specify a learning topic, their target proficiency level (Beginner, Moderate, Advanced), the goal they want to achieve, and the amount of time they can allocate daily for study. /n Each lesson plan must encompass: /n - A 'Topic' field detailing the primary focus for the day. /n - A 'Subtopics' field listing the particular subtopics to be addressed. /n *Keep the daily 'study-time' in mind when structuring the daily topics and subtopics to ensure the lesson plan fits within the allocated time.* /n Every lesson plan should commence with foundational knowledge of the subject, irrespective of the proficiency level. The Beginner plan should offer a succinct introduction, the Moderate plan a more comprehensive guide, and the Advanced plan an in-depth analysis of the subject. The complexity and detail of each plan should correspond to the user's desired level of mastery. **IMPORTANT NOTE:** /n - Beginner-level plans must be structured for more than 7 days with daily time in mind. /n - Moderate-level plans must be structured for more than 10 days with daily time in mind. /n - Advanced-level plans must be structured for more than 14 days with daily time in mind.",
 };
 
 const knowledgeCheckGenerationConfig = {
-  temperature: 1,
-  topP: 0.95,
-  topK: 64,
-  maxOutputTokens: 8192,
-  responseMimeType: "application/json",
-  responseSchema: {
-    type: "object",
-    properties: {
-      question: {
-        type: "array",
-        items: {
-          type: "object",
-          properties: {
-            question_text: {
-              type: "string",
-            },
-            options: {
-              type: "array",
-              items: {
+  model: "gemini-1.5-flash-exp-0827",
+  generationConfig: {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "application/json",
+    responseSchema: {
+      type: "object",
+      properties: {
+        question: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              question_text: {
+                type: "string",
+              },
+              options: {
+                type: "array",
+                items: {
+                  type: "string",
+                },
+              },
+              correct_answer: {
+                type: "string",
+              },
+              explanation: {
                 type: "string",
               },
             },
-            correct_answer: {
-              type: "string",
-            },
-            explanation: {
-              type: "string",
-            },
+            required: [
+              "question_text",
+              "options",
+              "correct_answer",
+              "explanation",
+            ],
           },
-          required: [
-            "question_text",
-            "options",
-            "correct_answer",
-            "explanation",
-          ],
         },
       },
+      required: ["question"],
     },
-    required: ["question"],
   },
+  systemInstruction:
+    "As an expert in educational content creation, your task is to generate a knowledge check in JSON format based on the provided lesson topic. The knowledge check should include a minimum of 8 and a maximum of 10 multiple-choice questions, with difficulty progressing from easy to hard. Each question should have the following structure: - 'question_text': The text of the question. - 'options': An array of four possible answers. - 'correct_answer': The index of the correct answer in the options array (0, 1, 2, or 3). - 'explanation': A brief explanation of why the correct answer is correct. The first few questions (1-3) should focus on basic concepts and be easier, while questions towards the end (8-10) should cover more advanced or nuanced topics, becoming progressively more challenging.",
 };
 
 const safetySettings = [
@@ -112,9 +122,9 @@ const safetySettings = [
 
 const generateContent = async (prompt, config) => {
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro-exp-0827",
-    generationConfig: config,
-    safetySettings,
+    model: config.model,
+    generationConfig: config.generationConfig,
+    safetySettings: safetySettings,
     systemInstruction: config.systemInstruction,
   });
 
@@ -127,6 +137,6 @@ const generateContent = async (prompt, config) => {
 };
 
 export const generateLessonPlan = (prompt) =>
-  generateContent(prompt, lessonGenerationConfig);
+  generateContent(prompt, lessonPlanGenerationConfig);
 export const generateKnowledgeCheck = (prompt) =>
   generateContent(prompt, knowledgeCheckGenerationConfig);
