5b278c8bddfae92fc3e60b283b3c61e6a6186ad2
Mayank Bavankar <mayanksahu1053@gmail.com>
2024-11-18 00:12:18 +0530

    Improve error logging in asyncHandler to capture and log errors before passing them to the next middleware

--- Backend/src/utils/AsyncHandler.js
+++ Backend/src/utils/AsyncHandler.js
@@ -1,7 +1,9 @@
 const asyncHandler = (fn) => {
   return (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-    console.log("err", err);
+    Promise.resolve(fn(req, res, next)).catch((err) => {
+      console.log(err);
+      next(err);
+    });
   };
 };
 
